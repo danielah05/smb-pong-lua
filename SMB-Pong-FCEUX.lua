7aaca990e194a47308f81c3335d1aa5ae9f5ff9a
danielh05 <daniel_henkel@yahoo.com>
2021-10-23 19:29:34 +0200

    add settings menu and two settings

--- SMB-Pong-FCEUX.lua
+++ SMB-Pong-FCEUX.lua
@@ -2,6 +2,8 @@
 --  PONG SMB - A LUA SCRIPT PORT FOR FCEUX  --
 --   created by danielah05 - @flstudiodemo  --
 ----------------------------------------------
+--  i really need to clean up this code :/  --
+----------------------------------------------
 local p1_x = 16
 local p1_y = 90
 local p1_width = 16
@@ -20,6 +22,13 @@ local p1_score = 0
 local p2_score = 0
 local p1_vs_score = 0
 local p2_vs_score = 0
+-- bunch of settings stuff do not touch (just like how you shouldnt touch anything in general)
+settings_menu_open = 0 -- should be 0
+settings_selected = 11+9 -- should be toggled to 11 on menu open
+settings_menu_delay = 0
+settings_pong_only = 0
+settings_hard_mode = 0
+----------------------------------------
 
 while true do
 	gamestarted = memory.readbyte(0x0770)
@@ -28,13 +37,72 @@ while true do
 	input = joypad.read(1)
 	input2 = joypad.read(2)
 	
+	mario_powerstate = memory.readbyte(0x0754)
+	mario_x = memory.readbyte(0x03AD)
+	if mario_powerstate == 0 then mario_y = memory.readbyte(0x03B8) end
+	if mario_powerstate == 1 then mario_y = memory.readbyte(0x03B8)+16 end
+	mario_width = 16
+	if mario_powerstate == 0 then mario_height = 32 end
+	if mario_powerstate == 1 then mario_height = 16 end
+	
 	if gamestarted == 0 then
 		gui.drawbox(0+20, 210, 0+20+256-40, 208+32-11, "#33000000")
 		gui.drawbox(1+20, 211, 1+20+254-40, 209+30-11, "#2264b0ff")
 		gui.drawtext(31, 217, "PONG SMB - A Lua Script by danielah05", "#ffffff")
+    
+    if input.B then
+      if settings_menu_delay == 0 then
+        settings_menu_open = settings_menu_open == 1 and 0 or 1
+        settings_menu_delay = 10
+      end
+    end
+    
+    if settings_menu_open == 0 then
+      settings_menu_delay = math.max(0, settings_menu_delay - 1)
+      gui.drawbox(0, 0+8, 114, 11+9, "#33000000")
+      gui.drawbox(1, 1+8, 1+112, 1+9+9, "#2264b0ff")
+      gui.drawtext(3, 2+9, "Settings - Open with B", "#ffffff")
+    end
+    
+    if settings_menu_open == 1 then
+      if settings_menu_delay == 0 then
+        if input.down then
+          settings_menu_delay = 10
+          settings_selected = math.min(21+9, settings_selected + 10+9)
+        end
+    
+        if input.up then
+          settings_menu_delay = 10
+          settings_selected = math.max(11+9, settings_selected - 10-9)
+        end
+        
+        if input.A then
+          settings_menu_delay = 20
+            if settings_selected == 11+9 then settings_pong_only = settings_pong_only == 1 and 0 or 1 end
+            if settings_selected == 21+9 then settings_hard_mode = settings_hard_mode == 1 and 0 or 1 end
+        end
+      end
+      
+      settings_menu_delay = math.max(0, settings_menu_delay - 1)
+      gui.drawbox(0, 0+8, 114, 11+9, "#33000000")
+      gui.drawbox(1, 1+8, 1+112, 1+9+9, "#2264b0ff")
+      gui.drawbox(0, 11+8, 114, 11+30-2+9, "#33000000")
+      gui.drawbox(1, 11+8, 1+112, 11+27+9, "#2264b0ff")
+      gui.drawbox(113, 0+8, 114+141, 39+9, "#33000000")
+      gui.drawbox(114, 1+8, 114+140, 1+37+9, "#2264b0ff")
+      gui.drawbox(1, settings_selected, 1+112, settings_selected+10, "#1100b0ff")
+      gui.drawtext(2, 2+9, "Settings - Close with B", "#ffffff")
+      gui.drawtext(2, 12+9, "Pong Only: " .. settings_pong_only, "#ffffff")
+      gui.drawtext(2, 22+9, "Hard Mode: " .. settings_hard_mode, "#ffffff")
+      if settings_selected == 11+9 then gui.drawtext(115, 2+9, "Disables anything SMB to\nallow a normal and fun game\nof Pong! (Includes 2 Player)", "#ffffff") end
+      if settings_selected == 21+9 then gui.drawtext(115, 2+9, "Tired of having fun?\nTry Hard Mode instead!\nDon't let the Ball get Mario,\nor something bad happends!", "#ffffff") end
+    end
 	end
 	
 	if gamestarted > 0 then
+	    if settings_pong_only == 1 then
+			memory.writebyte(0x000E, 0x08)
+		end
 	
 		local p1col = "#ff0000"
 		local p2col = "#00ff00"
@@ -56,10 +124,25 @@ while true do
 			p2_y = math.max(0, p2_y - 5)
 		end
 		
-		ball_x = ball_x + ball_dx
-		ball_y = ball_y + ball_dy
+		-- no hard mode
+		if settings_hard_mode == 0 then
+			ball_x = ball_x + ball_dx
+			ball_y = ball_y + ball_dy
+		end
+		-- hard mode
+		if settings_hard_mode == 1 then
+			ball_x = ball_x + ball_dx + ball_dx + ball_dx
+			ball_y = ball_y + ball_dy + ball_dy + ball_dy
+		end
 		
-		if (ball_y < 0 or ball_y > 231) then
+		if (ball_y < 0) then
+			ball_y = 0
+			memory.writebyte(0x00FF, 0x02)
+			ball_dy = -ball_dy
+		end
+		
+		if (ball_y > 231) then
+			ball_y = 231
 			memory.writebyte(0x00FF, 0x02)
 			ball_dy = -ball_dy
 		end
@@ -68,6 +151,12 @@ while true do
 			memory.writebyte(0x00FF, 0x02)
 			ball_x = p1_x+16
 			ball_dx = -ball_dx
+		end
+		
+		if settings_hard_mode == 1 then
+			if (mario_x < ball_x + ball_width and mario_x + mario_width > ball_x and mario_y < ball_y + ball_height and mario_y + mario_height > ball_y) then
+				memory.writebyte(0x000E, 0x06)
+			end
 		end
 		
 		if is2player == 0 then
@@ -82,10 +171,11 @@ while true do
 			p1_y = 90
 			ball_dx = math.random(2) == 1 and 2 or -2
 			ball_dy = math.random(2) == 1 and 2 or -2
-			emu.softreset()
+			if settings_pong_only == 0 then emu.softreset() end
 			end
 		
 			if ball_x > 248 then
+				ball_x = 248
 				p1_score = math.min(999, p1_score + 1)
 				memory.writebyte(0x00FF, 0x02)
 				ball_dx = -ball_dx
@@ -114,7 +204,7 @@ while true do
 				p2_y = 90
 				ball_dx = math.random(2) == 1 and 2 or -2
 				ball_dy = math.random(2) == 1 and 2 or -2
-				emu.softreset()
+				if settings_pong_only == 0 then emu.softreset() end
 			end
 		end
 		
@@ -131,6 +221,7 @@ while true do
 			gui.drawtext(10, 219, "P1: " .. p1_vs_score, p1col)
 			gui.drawtext(10*21, 219, "P2: " .. p2_vs_score, p2col)
 		end
+		gui.drawbox(mario_x, mario_y, mario_x+mario_width, mario_y+mario_height, "#FF000000",  "#FF000000")
 	end
 	emu.frameadvance()
 end
--- SMB-Pong-MESEN.lua
+++ SMB-Pong-MESEN.lua
@@ -2,6 +2,8 @@
 --  PONG SMB - A LUA SCRIPT FOR MESEN    --
 -- created by danielah05 - @flstudiodemo --
 -------------------------------------------
+-- i really need to clean up this code :/--
+-------------------------------------------
 p1_x = 16
 p1_y = 90
 p1_width = 16
@@ -20,6 +22,13 @@ p1_score = 0
 p2_score = 0
 p1_vs_score = 0
 p2_vs_score = 0
+-- bunch of settings stuff do not touch (just like how you shouldnt touch anything in general)
+settings_menu_open = 0 -- should be 0
+settings_selected = 11 -- should be toggled to 11 on menu open
+settings_menu_delay = 0
+settings_pong_only = 0
+settings_hard_mode = 0
+----------------------------------------
 
 function main()
   state = emu.getState()
@@ -30,13 +39,72 @@ function main()
   input = emu.getInput(0)
   input2 = emu.getInput(1)
   
+  mario_powerstate = emu.read(0x0754, emu.memType.cpu)
+  mario_x = emu.read(0x03AD, emu.memType.cpu)
+  if mario_powerstate == 0 then mario_y = emu.read(0x03B8, emu.memType.cpu) end
+  if mario_powerstate == 1 then mario_y = emu.read(0x03B8, emu.memType.cpu)+16 end
+  mario_width = 16
+  if mario_powerstate == 0 then mario_height = 32 end
+  if mario_powerstate == 1 then mario_height = 16 end
+  
   if gamestarted == 0 then
     emu.drawRectangle(0+20, 208+5, 256-40, 32-10, 0x33000000, true, 1)
     emu.drawRectangle(1+20, 209+5, 254-40, 30-10, 0x2264b0ff, true, 1)
     emu.drawString(31, 220, "PONG SMB - A Lua Script by danielah05", 0xffffff, 0xFF000000, 1)
+    
+    if input.b then
+      if settings_menu_delay == 0 then
+        settings_menu_open = settings_menu_open == 1 and 0 or 1
+        settings_menu_delay = 10
+      end
+    end
+    
+    if settings_menu_open == 0 then
+      settings_menu_delay = math.max(0, settings_menu_delay - 1)
+      emu.drawRectangle(0, 0, 114, 11, 0x33000000, true, 1)
+      emu.drawRectangle(1, 1, 112, 9, 0x2264b0ff, true, 1)
+      emu.drawString(2, 2, "Settings - Open with B", 0xffffff, 0xFF000000, 1)
+    end
+    
+    if settings_menu_open == 1 then
+      if settings_menu_delay == 0 then
+        if input.down then
+          settings_menu_delay = 10
+          settings_selected = math.min(21, settings_selected + 10)
+        end
+    
+        if input.up then
+          settings_menu_delay = 10
+          settings_selected = math.max(11, settings_selected - 10)
+        end
+        
+        if input.a then
+          settings_menu_delay = 20
+            if settings_selected == 11 then settings_pong_only = settings_pong_only == 1 and 0 or 1 end
+            if settings_selected == 21 then settings_hard_mode = settings_hard_mode == 1 and 0 or 1 end
+        end
+      end
+      
+      settings_menu_delay = math.max(0, settings_menu_delay - 1)
+      emu.drawRectangle(0, 0, 114, 11, 0x33000000, true, 1)
+      emu.drawRectangle(1, 1, 112, 9, 0x2264b0ff, true, 1)
+      emu.drawRectangle(0, 11, 114, 30-2, 0x33000000, true, 1)
+      emu.drawRectangle(1, 11, 112, 27, 0x2264b0ff, true, 1)
+      emu.drawRectangle(114, 0, 142, 39, 0x33000000, true, 1)
+      emu.drawRectangle(114, 1, 141, 37, 0x2264b0ff, true, 1)
+      emu.drawRectangle(1, settings_selected, 112, 10, 0x1100b0ff, true, 1)
+      emu.drawString(2, 2, "Settings - Close with B", 0xffffff, 0xFF000000, 1)
+      emu.drawString(2, 12, "Pong Only: " .. settings_pong_only, 0xffffff, 0xFF000000, 1)
+      emu.drawString(2, 22, "Hard Mode: " .. settings_hard_mode, 0xffffff, 0xFF000000, 1)
+      if settings_selected == 11 then emu.drawString(115, 2, "Disables anything SMB to\nallow a normal and fun game\nof Pong! (Includes 2 Player)", 0xffffff, 0xFF000000, 1) end
+      if settings_selected == 21 then emu.drawString(115, 2, "Tired of having fun?\nTry Hard Mode instead!\nDon't let the Ball get Mario,\nor something bad happends!", 0xffffff, 0xFF000000, 1) end
+    end
   end
  
   if gamestarted > 0 then
+    if settings_pong_only == 1 then
+      emu.write(0x000E, 0x08, emu.memType.cpu)
+    end
     
     p1col = 0xff0000
     p2col = 0x00ff00
@@ -59,10 +127,19 @@ function main()
         p2_y = math.max(0, p2_y - 5)
       end
     end
-     
-    ball_x = ball_x + ball_dx
-    ball_y = ball_y + ball_dy
-      
+    
+    -- no hard mode
+    if settings_hard_mode == 0 then
+      ball_x = ball_x + ball_dx
+      ball_y = ball_y + ball_dy
+    end
+    -- hard mode
+    if settings_hard_mode == 1 then
+      ball_x = ball_x + ball_dx + ball_dx + ball_dx
+      ball_y = ball_y + ball_dy + ball_dy + ball_dy
+    end
+    
+    
     if (ball_y < 0 or ball_y > 231) then
       emu.write(0x00FF, 0x02, emu.memType.cpu)
       ball_dy = -ball_dy
@@ -72,6 +149,12 @@ function main()
       emu.write(0x00FF, 0x02, emu.memType.cpu)
       ball_x = p1_x+16
       ball_dx = -ball_dx
+    end
+    
+    if settings_hard_mode == 1 then
+      if (mario_x < ball_x + ball_width and mario_x + mario_width > ball_x and mario_y < ball_y + ball_height and mario_y + mario_height > ball_y) then
+        emu.write(0x000E, 0x06, emu.memType.cpu)
+      end
     end
     
     if is2player == 0 then
@@ -86,7 +169,7 @@ function main()
         p1_y = 90
         ball_dx = math.random(2) == 1 and 2 or -2
         ball_dy = math.random(2) == 1 and 2 or -2
-        emu.reset()
+        if settings_pong_only == 0 then emu.reset() end
       end
       
       if ball_x > 248 then
@@ -118,7 +201,7 @@ function main()
         p2_y = 90
         ball_dx = math.random(2) == 1 and 2 or -2
         ball_dy = math.random(2) == 1 and 2 or -2
-        emu.reset()
+        if settings_pong_only == 0 then emu.reset() end
       end
     end
     
@@ -137,6 +220,7 @@ function main()
       emu.drawString(10, 225, "P1: " .. p1_vs_score, p1col, 0xFF000000, 1)
       emu.drawString(10*21, 225, "P2: " .. p2_vs_score, p2col, 0xFF000000, 1)
     end
+    emu.drawRectangle(mario_x, mario_y, mario_width, mario_height, 0xFF000000, true, 1)
   end
 end
 
